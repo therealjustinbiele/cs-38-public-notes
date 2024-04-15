3d46a1f4e67d53973fcb030da92b4f9dc162e98a
Justin Biele <justin.biele@gmail.com>
2024-04-15 15:59:42 -0700

    add mouse example

--- apr-15/MouseEx.java
+++ apr-15/MouseEx.java
@@ -9,13 +9,23 @@ public class MouseEx
     public static void mouse1()
     {
         Draw screen = new Draw();
-        double x, y;
-        for (int count = 0; count < 20; count++)
+        double x = 0;
+        double y;
+        int count = 0;
+        while (x < 0.99)
         {
-            screen.pause(500);
-            x = screen.mouseX();
-            y = screen.mouseY();
-            System.out.println(String.format("x: %.3f, y: %.3f", x, y));
+            screen.pause(250);
+            if (screen.isMousePressed())
+            {
+                System.out.printf("Mouse is being clicked...");
+                x = screen.mouseX();
+                y = screen.mouseY();
+                screen.setPenColor(Draw.BLUE);
+                screen.filledCircle(x, y, 0.05);
+                System.out.printf("%d: x: %.3f, y: %.3f \n", count, x, y);
+            }
+            count++;
         }
+        System.out.println("\n Program exited");
     }
 }
